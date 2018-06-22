c6be307b84b94d3b2b82779d554a5b02b562da69
Roliver Javier Rodriguez <roliverjavier@gmail.com>
2018-06-22 10:30:22 -0400

    upload

---
+++ 06-Exploring the HTTP Object/server-upload.js
@@ -0,0 +1,21 @@
+var fs = require('fs'),
+    queryString = require('querystring'),
+    util = require('util'),
+    bodyParser = require('body-parser'),
+    form = fs.readFileSync('form.html'),
+    connect = require('connect');
+
+    connect(connect.limit('2mb'),
+            bodyParser(),function(request, response){
+        switch(request.method){
+            case 'POST':
+                console.log('User Posted:\n', request.body);
+                response.end('You posted:\n'+ util.inspect(request.body));
+            break;
+
+            case 'GET':
+                response.writeHead(200, {'Content-type': 'text/html'});
+                response.end();
+            break;
+        }
+    }).listen(3001);
